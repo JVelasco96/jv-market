019c9f0fd36197a187b861046f05d6aced373719
Javier Antonio Velasco Reyes <javier.velascor@coppel.com>
2022-11-15 09:56:35 -0700

    Se Agrga el servicio para compra y el contorlador con los metodos para su manipulacion

--- jv-market/src/main/java/com/jv/market/web/controller/PurchaseController.java
+++ jv-market/src/main/java/com/jv/market/web/controller/PurchaseController.java
@@ -1,2 +1,38 @@
-package com.jv.market.web.controller;public class PurchaseController {
+package com.jv.market.web.controller;
+
+//import ch.qos.logback.core.net.server.Client;
+import com.jv.market.domain.Purchase;
+import com.jv.market.domain.service.PurchaseService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchases")
+public class PurchaseController {
+
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Purchase>> getAll(){
+        return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/client/{clientId}")
+    public ResponseEntity<List<Purchase>> getByClientId(@PathVariable("clientId") String clientId){
+       return purchaseService.getByClient(clientId)
+               .map(purchases -> new ResponseEntity<>(purchases, HttpStatus.OK))
+               .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
+        return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
+    }
 }
